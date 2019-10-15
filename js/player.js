6d0ff15760cad1c09b5d45c2143741a68e797e1e
Doctorbingbing <christianparizeau@gmail.com>
2019-10-15 12:31:30 -0700

    Checking if player can play card and playing card

--- js/player.js
+++ js/player.js
@@ -1,26 +1,38 @@
 class Player {
-  constructor(){
-    this.name = '';
+  constructor() {
+    this.name = "";
     this.canPlay = false;
     this.resources = {};
     this.victoryPoints = 0;
     this.cardsInHand = [];
-
   }
 
-    playCard( cardToPlay ) { // expects a Card object
-
+  playCard(cardToPlay) {
+    // expects a Card object
+    if (this.canPlay(cardToPlay)) {
+      this.resources.money -= cardToPlay.cost;
+    } else {
+      return false;
     }
+  }
 
-    getResource ( resourceType, resourceAmount ) { // expects string, number
+  getResource(resourceType, resourceAmount) {
+    // expects string, number
+  }
 
+  setResource(resourceType, resourceAmount) {
+    // expects string, number
+  }
+
+  addCardtoHand(cardtoAdd) {
+    // expects a Card object
+  }
+
+  canPlay(cardToPlay) {
+    if (cardToPlay.cost > this.resources.money.currentValue) {
+      return false;
+    } else {
+      return true;
     }
-
-    setResource ( resourceType, resourceAmount ){ // expects string, number
-
-    }
-
-    addCardtoHand ( cardtoAdd ){ // expects a Card object
-
-    }
+  }
 }
