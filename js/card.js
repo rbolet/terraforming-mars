36f5ffb1e070d93ea32de5f4b32e5a6580a343cc
Doctorbingbing <christianparizeau@gmail.com>
2019-10-15 15:21:34 -0700

    Added render to card

--- js/card.js
+++ js/card.js
@@ -1,7 +1,9 @@
 class Card {
   constructor(cost, type, changeRate, changeVal) {
+    //Accepts cost as number, type as string, changeRate as number, and changeVal as number
     this.causeEffect = this.causeEffect.bind(this);
     this.cost = cost;
+    this.element;
     (this.type = type),
       (this.valuesToChange = {
         changeRate: changeRate,
@@ -11,5 +13,28 @@ class Card {
 
   causeEffect(player) {
     game.changeResource(player, this.effect.type, this.valuesToChange);
+  }
+
+  render() {
+    var newDiv = $("<div>").addClass("card");
+    var cost = $("<div>")
+      .addClass("cost")
+      .text(this.cost);
+    var effect = $("<div>").addClass("effects");
+    var rateChangeText =
+      this.type +
+      " production will change by " +
+      this.valuesToChange.changeRate +
+      " per generation";
+    var valueChangeText =
+      "Your " + this.type + " will change by " + this.valuesToChange.changeVal;
+    effect.append(
+      $("<div>").text(rateChangeText),
+      $("<div>").text(valueChangeText)
+    );
+    newDiv.append(cost, effect);
+    newDiv.on("click", tryToPlace);
+    this.element = newDiv;
+    return newDiv;
   }
 }
--- js/game.js
+++ js/game.js
@@ -3,30 +3,98 @@ class Game {
     this.currentOxygen = 0;
     this.currentTemperature = -30;
     this.currentGeneration = 1;
-    this.currentPhase = "";
+    this.currentPhase = 0;
     this.phaseList = ["Research", "Action", "Production"];
-    this.cardsInDeck = [];
+    this.currentPlayer = 0;
     this.playerList = [];
-    this.addCard();
   }
-  get oxygen() {}
-  set oxygen(numToAdvance) {}
-  get temperature() {}
-  set temperature(numToAdvance) {}
-  get generation() {}
-  set generation(numToAdvance) {}
-  get phase() {}
-  advancePhase() {}
-  changeRescource(player, type, numToChange) {}
+  get oxygen() {
+    return this.currentOxygen;
+  }
+  set oxygen(numToAdvance) {
+    // expects number
+    this.currentOxygen += numToAdvance;
+    if (this.currentOxygen > 14) {
+      this.currentOxygen = 14;
+      return false;
+    }
+    return true;
+  }
+  get temperature() {
+    return this.currentTemperature;
+  }
+  set temperature(numToAdvance) {
+    //expects number
+    this.currentTemperature += numToAdvance;
+    if (this.currentTemperature > 8) {
+      this.currentTemperature = 8;
+      return false;
+    }
+    return true;
+  }
+  get generation() {
+    return this.currentGeneration;
+  }
+  set generation(numToAdvance) {
+    //expects number
+    this.currentGeneration += numToAdvance;
+    if (this.currentGeneration > 100) {
+      this.currentGeneration = 100;
+      return false;
+    }
+    return true;
+  }
+  get phase() {
+    return this.currentPhase;
+  }
+  advancePhase() {
+    if (this.currentPhase === 2) {
+      this.currentPhase = 0;
+    } else {
+      this.currentPhase++;
+    }
+  }
+  changeResource(player, typeToChange, valuesToChange) {
+    //expects number, string, object
+    var playerToChange = this.playerList[player];
+    var resourceToChange = this.playerToChange.resource[typeToChange];
+
+    playerToChange.resourceToChange.currentValue += valuesToChange.currentValue;
+    playerToChange.resourceToChange += valuesToChange.rate;
+  }
   researchPhase() {}
   actionPhase() {}
   productionPhase() {}
-  advanceTurn() {}
+  advanceTurn() {
+    this.currentPlayer++;
+    if (this.currentPlayer === this.playerList.length) {
+      this.currentPlayer = 0;
+    }
+  }
   dealCard() {}
-  shuffleCards() {}
+  shuffleCards() {
+    var newPos = 0;
+    var tempVar = 0;
+    for (
+      var position = this.cardsInDeck.length - 1;
+      position >= 0;
+      position--
+    ) {
+      newPos = Math.floor(Math.random() * (position + 1));
+      tempVar = this.cardsInDeck[position];
+      this.cardsInDeck[position] = this.cardsInDeck[newPos];
+      this.cardsInDeck[newPos] = tempVar;
+    }
+    return this.cardsInDeck;
+  }
 
   addCard() {
     var newCard = new Card(15, "plants", 5, 2);
     this.cardsInDeck.push(newCard);
+  }
+
+  addPlayer(name) {
+    // expects string
+    var newPlayer = new Player(name);
   }
 }
