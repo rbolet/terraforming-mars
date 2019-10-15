d1927b3311d5a615f74551443b1c643b4c9d101d
Rapha B <Rapha B>
2019-10-15 10:39:46 -0700

    JavaScript Class skeletons created Class skeletons for Player and Board

---
+++ js/board.js
@@ -0,0 +1,14 @@
+class Board {
+  constructor(){
+    this.boardArray = []; // array of row arrays containing space arrays, containing tile objects
+
+  }
+
+  checkForTile( positionToCheck ){ // expects a position <- TBD how to store/pass positions
+
+  }
+
+  placeTile( tiletoPlace, positionToPlace){ // expects Tile object, position <-TBD how to store/pass positions
+
+  }
+}
