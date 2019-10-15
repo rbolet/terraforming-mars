64b1a7c3abeda58963ea4e2564dad9110131da68
Roger Enand <rogerenand@gmail.com>
2019-10-15 12:40:13 -0700

    added values to board array and started the place tile

--- js/board.js
+++ js/board.js
@@ -1,7 +1,10 @@
 class Board {
-  constructor(){
-    this.boardArray = []; // array of row arrays containing space arrays, containing tile objects
-
+  constructor(){//        0     1     2     3     4
+    this.boardArray = [ [null, null, null, null, null], //0
+                        [null, null, null, null, null], //1
+                        [null, null, null, null, null], //2
+                        [null, null, null, null, null], //3
+                        [null, null, null, null, null]];//4 // array of row arrays containing space arrays, containing tile objects
   }
 
   checkForTile( positionToCheck ){ // expects a position <- TBD how to store/pass positions
@@ -9,6 +12,11 @@ class Board {
   }
 
   placeTile( tiletoPlace, positionToPlace){ // expects Tile object, position <-TBD how to store/pass positions
-
+    if (this.boardArray[positionToPlace.row][positionToPlace.column]!==null){//can place there
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 }
