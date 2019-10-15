8d6eaca288e7e202aa4178f4fd49936b27235ff7
Rapha B <Rapha B>
2019-10-15 14:21:10 -0700

    added render method to Tile class

--- js/tile.js
+++ js/tile.js
@@ -1,6 +1,16 @@
 class Tile {
-  constructor(){
-    this.type = "";
-    this.owner = "";
+  constructor( tileType, tileOwner ){ // expects string "greenery" or "city" , player object
+    this.type = tileType;
+    this.owner = tileOwner;
+    this.tileElement = {}; // jQuery object
   }
+
+  render(){
+    var newTile = $("<div>")
+    newTile.addClass("tile " + this.type);
+
+    this.tileElement = newTile;
+    return this.tileElement;
+    }
+
 }
