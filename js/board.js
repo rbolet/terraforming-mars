faefe80febc0101b0a048fe807745475c13cdeb1
Doctorbingbing <christianparizeau@gmail.com>
2019-10-15 16:46:22 -0700

    Added click handler to game grid added position property for each tile

--- js/board.js
+++ js/board.js
@@ -1,21 +1,46 @@
 class Board {
-  constructor(){//        0     1     2     3     4
-    this.boardArray = [ [null, null, null, null, null], //0
-                        [null, null, null, null, null], //1
-                        [null, null, null, null, null], //2
-                        [null, null, null, null, null], //3
-                        [null, null, null, null, null]];//4 // array of row arrays containing space arrays, containing tile objects
+  constructor() {
+    //        0     1     2     3     4
+    this.boardArray = [
+      [null, null, null, null, null], //0
+      [null, null, null, null, null], //1
+      [null, null, null, null, null], //2
+      [null, null, null, null, null], //3
+      [null, null, null, null, null]
+    ]; //4 // array of row arrays containing space arrays, containing tile objects
+    this.populateArray();
+    this.addClickHandler();
   }
-
-  checkForTile( positionToCheck ){ // expects a position <- TBD how to store/pass positions
-
-  }
-
-  placeTile( tiletoPlace, positionToPlace){ // expects Tile object, position <-TBD how to store/pass positions
-    if (this.boardArray[positionToPlace.row][positionToPlace.column]!==null){//can place there
-      return true;
+  populateArray() {
+    for (var row = 0; row < 5; row++) {
+      var currentRow = $("<div>").addClass("row");
+      for (var cell = 0; cell < 5; cell++) {
+        var newTile = new Tile("empty", null, { row: row, cell: cell });
+        this.boardArray[row][cell] = newTile;
+        newTile.render();
+        currentRow.append(newTile.tileElement);
+      }
+      $(".board").append(currentRow);
     }
-    else {
+  }
+  addClickHandler() {
+    $(".board").on("click", $(".cell"), function() {
+      console.log("A cell was clicked");
+    });
+  }
+
+  checkForTile(positionToCheck) {
+    // expects a position <- TBD how to store/pass positions
+  }
+
+  //Add class of legal moves to legal spots. Add click delegator for the gameboard that looks for a legal moved to be clicked
+
+  placeTile(tiletoPlace, positionToPlace) {
+    // expects Tile object, position <-TBD how to store/pass positions
+    if (this.boardArray[positionToPlace.row][positionToPlace.column] !== null) {
+      //can place there
+      return true;
+    } else {
       return false;
     }
   }
