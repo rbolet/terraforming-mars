5908b66a2fcd47b856e04012a1611241ab8d5a80
Roger Enand <rogerenand@gmail.com>
2019-10-15 10:39:35 -0700

    Created skeleton of game.js and added intitialize to main.js

---
+++ js/game.js
@@ -0,0 +1,56 @@
+class Game{
+  constructor(){
+    this.currentOxygen = 0;
+    this.currentTemprature = -30;
+    this.currentGeneration = 1;
+    this.currentPhase = '';
+    this.phaseList = ['Research','Action','Production'];
+    this.cardsInDeck = [];
+    this.playerList =[];
+  }
+  get o2(){
+
+  }
+  set o2(numToAdvance){
+
+  }
+  get temprature(){
+
+  }
+  set temprature(numToAdvance){
+
+  }
+  get generation(){
+
+  }
+  set generation(numToAdvance){
+
+  }
+  get phase(){
+
+  }
+  advancePhase(){
+
+  }
+  changeRescource(player,type,numToChange){
+
+  }
+  researchPhase(){
+
+  }
+  actionPhase(){
+
+  }
+  productionPhase(){
+
+  }
+  advanceTurn(){
+
+  }
+  dealCard(){
+
+  }
+  shuffleCards(){
+
+  }
+}
