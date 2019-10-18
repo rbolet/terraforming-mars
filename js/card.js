70f441b8a5465e72e5f5c5f57e07db069775bfc1
Rapha B <Rapha B>
2019-10-17 17:27:44 -0700

    Cards can removed, tested

--- js/card.js
+++ js/card.js
@@ -1,22 +1,17 @@
 class Card {
-  constructor(
-    cost,
-    type,
-    tileToPlace,
-    permanentBool,
-    gameClickHandler,
-    gameRemoveDiv
-  ) {
+  constructor(cost,type,tileToPlace,permanentBool,gameClickHandler,gameRemoveDiv) {
     //Accepts cost as number, type as string,
     this.causeEffect = this.causeEffect.bind(this);
     this.handleClick = this.handleClick.bind(this);
-    //this.playCard = this.playCard.bind(this);
+    // this.removeMe = this.removeMe.bind(this);
+    // this.playCard = this.playCard.bind(this);
     this.element = null; //jQuery object of the div
     this.cost = cost;
     this.typeObj = type;
     this.permanent = permanentBool;
     this.tileToPlace = tileToPlace;
     this.iWasClicked = gameClickHandler;
+    this.iNeedtoBeRemoved = gameRemoveDiv;
   }
 
   //Card needs to cll board to show valid placements
@@ -24,12 +19,19 @@ class Card {
     return this.tileToPlace;
   }
   handleClick() {
-    this.iWasClicked(this);
+    // this.iWasClicked(this);
+    this.iNeedtoBeRemoved(this.element);
   }
   causeEffect() {
     for (var effect of this.typeObj)
       game.changeResource(effect.type, effect.effects.resourcesvaluesToChange); //Should be from callbacks
   }
+
+  // removeMe(){
+  //   if(!this.permanent) return false;
+
+  //   this.iNeedtoBeRemoved(this.element);
+  // }
 
   render() {
     var newDiv = $("<div>")
